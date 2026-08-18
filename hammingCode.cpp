0e60fbba16e22283d99a7fa0afcdefc7b848e607
Aaryan89 <kumbhareaaryan@gmail.com>
2026-08-18 22:53:13 +0530

    some updates

--- hammingCode.cpp
+++ hammingCode.cpp
@@ -2,6 +2,33 @@
 #include <vector>
 using namespace std;
 
+void decodeAndCorrectHammingCode(vector<int> arr) {
+    cout << "\n--- RECEIVER SIDE ---" << endl;
+
+    int c1 = (arr[0] + arr[2] + arr[4] + arr[6]) % 2;
+    int c2 = (arr[0] + arr[1] + arr[4] + arr[5]) % 2;
+    int c3 = (arr[0] + arr[1] + arr[2] + arr[3]) % 2;
+
+    int syndrome = (c3 * 4) + (c2 * 2) + (c1 * 1);
+
+    if (syndrome == 0) {
+        cout << "Status: No error detected in transmission." << endl;
+    } else {
+        cout << "Status: Error detected at Bit Position (from right): " << syndrome << endl;
+        
+        int errorIndex = 7 - syndrome;
+        arr[errorIndex] = 1 - arr[errorIndex];
+        
+        cout << "Error corrected successfully." << endl;
+    }
+
+    int extractedData = (arr[0] * 1000) + (arr[1] * 100) + (arr[2] * 10) + arr[4];
+    
+    cout << "Reconstructed Codeword: ";
+    for(int b : arr) cout << b;
+    cout << "\nExtracted Original Data (Binary): " << extractedData << endl;
+}
+
 void encode4BitDataUsingHammingCode(int bin, int len) {
     vector<int> arr(len, 0);
     //arr[6] = -1, arr[5] = -1, arr[3] = -1;
@@ -18,7 +45,7 @@ void encode4BitDataUsingHammingCode(int bin, int len) {
     for(int i = 0; i < 7; i++) {
         ans += arr[i]; ans *= 10;
     } ans /= 10;
-    cout << "The final transmitted codeword is: " << ans;
+    cout << "The final transmitted codeword is: " << ans << endl;
 
     int cBit;
     cout << "Enter the bit you want to currupt: (-1 for clean) : "; cin >> cBit;
@@ -32,6 +59,8 @@ void encode4BitDataUsingHammingCode(int bin, int len) {
         } ans /= 10;
         cout << "The final transmitted codeword WITH ERROR is: " << ans;
     }
+
+    decodeAndCorrectHammingCode(arr);
 }
 
 int main() {
